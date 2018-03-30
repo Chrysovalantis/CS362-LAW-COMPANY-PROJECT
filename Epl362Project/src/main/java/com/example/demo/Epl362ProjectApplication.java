91a25cebfff6b96d587de6464b6af480caef3cf7
unknown <cchris47@cs.ucy.ac.cy>
2018-03-30 12:48:25 +0300

    Initial sample of a form

---
+++ Epl362Project/src/main/java/com/example/demo/Epl362ProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epl362ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epl362ProjectApplication.class, args);
+	}
+}
