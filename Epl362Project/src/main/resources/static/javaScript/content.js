69e27fd00be25f94fe7436d5dd5e4546a95d19cd
Rafaa17 <randre07@cs.ucy.ac.cy>
2018-04-25 23:37:56 +0300

    Changes

--- Epl362Project/src/main/resources/static/javaScript/content.js
+++ Epl362Project/src/main/resources/static/javaScript/content.js
@@ -1,23 +1,35 @@
+$(document).ready(function () {
+
+  $('#pencilRec').on('click', function () {
+    $("#recDescription").toggle('slow');
+  });
 
 
 
-$(document).ready(function(){
+  $('#pencilLeg').on('click', function () {
+    $("#opinionDescription").toggle('slow');
+  });
 
-    $('#pencilRec').on('click',function(){
-    	$("#recDescription").toggle('slow');
-    });
+  $(".btn-appointment").on('click', function () {
+    var appId = $(this).attr('name');
+    window.location.replace("http://localhost:8080/consultation/" + appId);
+  });
 
-
-
-    $('#pencilLeg').on('click',function(){
-    	$("#opinionDescription").toggle('slow');
-    });
-
-    $(".btn-appointment").on('click',function(){
-      var appId=$(this).attr('name');
-      window.location.replace("http://localhost:8080/consultation/"+appId);
-    });
-    
 });
 
 //CALENDAR
+
+var clientToChange;
+
+$(document).ready(function () {
+  $('#editClient').change(function () {
+    $('#changeClient').prop("disabled", false);
+    clientToChange=this.value;
+    var fullname=$("#editClient option:selected").text();
+    var names=fullname.split(' ');
+    var name=names[0];
+    var surname=names[1];
+    $('#clientNewName').val(name);
+    $('#clientNewSurname').val(surname);
+  });
+});
